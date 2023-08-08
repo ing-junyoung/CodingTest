ecf61e77167e66308adbccffda1fe15bf1d405e7
LeeJunyoung <99002540+ing-junyoung@users.noreply.github.com>
2023-08-08 13:09:39 +0900

    Create # 2884번.py

---
+++ 백준/# 2884번.py
@@ -0,0 +1,18 @@
+H , M = map(int , input().split())
+
+# 45분을 기준점으로 잡자. 
+
+if M < 45 :
+    
+    if H == 0 :
+        
+        print(23 , M + 15)
+    
+    else :
+        
+        print(H -1 , M + 15)
+        
+
+
+else :
+    print(H , M - 45)
