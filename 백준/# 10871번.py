d7550550b31db9f2ad338ae3460ed43c4ac82f1b
LeeJunyoung <99002540+ing-junyoung@users.noreply.github.com>
2023-08-09 22:09:56 +0900

    Create # 10871번.py

---
+++ 백준/# 10871번.py
@@ -0,0 +1,14 @@
+# 10871 
+
+N , X = map(int , input().split())
+
+A = list(map(int , input().split()))
+
+
+
+for i in A:
+    
+    if X > i :
+        print(i , end = ' ')
+    else:
+        pass
