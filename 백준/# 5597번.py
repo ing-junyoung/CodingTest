5b78b4eae1804fda268c2fec3b00bcce7f4ee664
LeeJunyoung <99002540+ing-junyoung@users.noreply.github.com>
2023-08-09 22:11:03 +0900

    Create # 5597번.py

---
+++ 백준/# 5597번.py
@@ -0,0 +1,15 @@
+# 5597
+
+
+total_num = [*range(1,31)]
+
+
+while True:
+    try:
+        total_num.remove(int(input()))
+    except:
+        break
+        
+        
+print(total_num[0])
+print(total_num[1])
